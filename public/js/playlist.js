9a328a1471b26c7b45dc431883c40c7f76385f3a
Corey Yaspan <cspan00@gmail.com>
2015-12-10 13:52:39 -0700

    add albums functionality enabled

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -7,30 +7,32 @@ $(document).ready(function(){
   })
 
   api.done(function(data){
-    console.log(data);
-
-
-
 
     for (var i = 0; i < data['results'].length; i++) {
       var images = "../images/"+ data['results'][i]['cover_art']
       var id =  data['results'][i]['id']
-      $("#album_scroll").append('<div class = album_cover id="'+ id+ '"><img src ="'+ images +'"/></div>');
-      // console.log(id);
-      // console.log(images);
+      $("#album_scroll").append('<div class = clickable  id="'+ id+ '"><img src ="'+ images +'"/></div>');
+
     };
 
-// <div class = album_ id="blah"><img src =""/></div>
+
+
+$(document).on("click", ".clickable", function(){
+        album_id = $(this).attr('id');
+    for(var j=0; j<data['results'].length; j++){
+
+      if(data['results'][j]['id'] == album_id){
+        var artist_album = data['results'][j]['artist'] + data['results'][j]['title']
+        $("#output").append(artist_album+'<br>');
+
+    }
+}
+  });
+
+
 
 
 });
-
-
-
-
-
-
-
 
 
 
